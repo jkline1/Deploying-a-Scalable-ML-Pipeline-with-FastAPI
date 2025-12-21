79e6480c8b0cd96692d1561b3946f129c0913d94
jaredhkline79-wgu <jaredhkline@yahoo.com>
2025-12-21 15:28:41 -0600

    figured out the inference, I think

--- main.py
+++ main.py
@@ -70,5 +70,5 @@ async def post_inference(data: Data):
         training=False,
         encoder=encoder   
     )
-    _inference = None # your code here to predict the result using data_processed
+    _inference = inference(model, data_processed)
     return {"result": apply_label(_inference)}
